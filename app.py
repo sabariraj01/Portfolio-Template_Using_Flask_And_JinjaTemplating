7bb4c28e7124c16703611c2ff6288ab99bb998f9
S Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-11-14 00:14:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',title='Sabari\'s PortFolio')
+
+@app.route('/Contact', methods=['GET'])
+def contact():
+    return render_template('contact.html',title='Sabari\'s PortFolio')
+
+@app.route('/About')
+def about():
+    return render_template('about.html',title='Sabari\'s PortFolio')
+
+@app.route('/Projects')
+def project():
+    return render_template('projects.html',title='Sabari\'s PortFolio')
+
+if __name__ == '__main__':
+    app.run(debug=True)
