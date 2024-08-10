aadadc88aabcec6dc51a706db13f3d5e6844706e
S Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2024-08-10 23:47:00 +0530

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -19,4 +19,4 @@ def projects():
     return render_template('projects.html', title="Sabari's Projects")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
